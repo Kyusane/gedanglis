30319fc34d8b44530e4b7d4f621316ce645e078d
Rahmat Rohmani <rahmatrohmani21@gmail.com>
2023-11-17 15:46:07 +0700

    change alamat monitoring

--- src/components/CircularBar.jsx
+++ src/components/CircularBar.jsx
@@ -26,7 +26,7 @@ const CircularBar= () => {
      },[])
 
      const getMonitoring = async ()=>{
-          const response = await fetch('http://localhost:3000/api/monitoring')
+          const response = await fetch('https://gedanglis.vercel.app/api/monitoring')
           const result = await response.json()
           const datas = result.monitoring
           setValue({
